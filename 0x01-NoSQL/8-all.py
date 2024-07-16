f93303fa5ec2a84554b799190d5cb3a66f527d23
Danielyilma <yilmad752@gmail.com>
2024-07-16 11:40:35 +0300

    NOSQL

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+'''list all document in a collection'''
 
 def list_all(mongo_collection):
+    '''function that return list of documents'''
     result = []
 
     for collection in mongo_collection.find():
