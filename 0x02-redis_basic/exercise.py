c03afeda75b60e138cf09263ee5d4cb1fbbfc525
Danielyilma <yilmad752@gmail.com>
2024-07-17 20:16:58 +0300

    redis database

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''creating a cache with redis database'''
+import redis
+from typing import Union
+import uuid
+
+class Cache:
+    '''cache class'''
+
+    def __init__(self):
+        '''initialize class attribute'''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[int, float, str]) -> str:
+        '''stores in redis database'''
+        random_num = str(uuid.uuid4())
+        self._redis.set(random_num, data)
+        return random_num
